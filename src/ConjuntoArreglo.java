4beb5800d7a6d2c7aaa20d00ac6b5015b7b202f3
JoséLuisHT <joseluisht@ciencias.unam.mx>
2025-09-09 14:39:54 -0600

    finish

--- src/ConjuntoArreglo.java
+++ src/ConjuntoArreglo.java
@@ -1,8 +1,14 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
+/**
+ * Clase que representa una estructura de datos {@link Conjunto} hecha con arreglos.
+ * La clase puede hacer las mismas acciones que un {@link Conjunto} como uniones o 
+ * intersecciones con otros conjuntos, saber si un elemento esta contenido en el 
+ * conjunto, etc.
+ * La clase extiende (obviamente) de la clase {@link Conjunto}.
+ */
 public class ConjuntoArreglo<T> extends Conjunto<T>{
-
+    
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
@@ -23,12 +29,20 @@ public class ConjuntoArreglo<T> extends Conjunto<T>{
         };
     }
 
-    public T[] elementos;
+    public T[] elementos; /*Los elementos del conjunto en un arreglo. */
 
+    /**
+     * Constructor que inicializa un objeto de tipo {@link ConjuntoArreglo} vacío.
+     */
     public ConjuntoArreglo() {
         this.elementos = (T[]) new Object[0];
     }
 
+    /** 
+     * Constructor que inicializa un objeto de tipo {@link ConjuntoArreglo}
+     * con los elementos pasados como argumentos.
+     * @param elementos El arreglo con los elementos a convertir en un {@link ConjuntoArreglo}.
+    */
     public ConjuntoArreglo(T[] elementos) {
         if (elementos == null) {
             this.elementos = (T[]) new Object[0];
@@ -41,6 +55,15 @@ public class ConjuntoArreglo<T> extends Conjunto<T>{
         }
     }
 
+    /**
+     * Método que nos dice si un elemento de tipo {@code T} pertenece al
+     * {@link ConjuntoArreglo}. Si el elemento pertenece al Conjunto devuelve
+     * {@code true} de otro modo devuelve {@code false}
+     * @param elemento El elemento de tipo {@code T} a vérificar si existe
+     * en el Conjunto.
+     * @return {@code true} Si el elemento pertenece al Conjunto. {@code false}
+     * de otro modo.
+     */
     public boolean pertenece(T elemento) {
         for(T e: elementos){
             if(e.equals(elemento)){
@@ -51,6 +74,15 @@ public class ConjuntoArreglo<T> extends Conjunto<T>{
         return false;
     }
 
+    /**
+     * Método que agrega un objeto elemento de tipo {@code T}
+     * al {@link ConjuntoArreglo} del mismo tipo (Si no son del mismo
+     * tipo ocurré un error).
+     * Como un Conjunto no tiene orden entonces el método agrega el
+     * elemento en el último espacio del {@code ConjuntoArreglo}.
+     * @param elemento El elemento de tipo {@code T} a agregar en el
+     * {@link ConjuntoArreglo}
+     */
     public void agregarElemento(T elemento) {
         if (pertenece(elemento)) {
             return;
@@ -63,6 +95,15 @@ public class ConjuntoArreglo<T> extends Conjunto<T>{
         elementos = nuevosElem;
     }
 
+    /**
+     *Método que nos dice si un {@link Conjunto} está contenido
+     en otro. Si el Conjunto está contenido entonces devuelve
+     {@code true}, de otra forma devuelve {@code false}.
+     @param c El conjunto génerico de tipo {@code T} a saber
+     si está contenido.
+     @return {@code true} Si el conjunto está contenido. De
+     otra forma {@code false}.
+     */
     public boolean contieneConjunto(Conjunto<T> c) {
         for (T t : c) {
             if (!pertenece(t)) {
@@ -72,6 +113,16 @@ public class ConjuntoArreglo<T> extends Conjunto<T>{
         return true;
     }
 
+    /**
+     * Método que devuelve un objeto de tipo {@link ConjuntoArreglo}
+     * que contiene la únión de los dos {@link Conjunto}.
+     * El método recibe un Conjunto génerico de tipo {@code T} y hace
+     * la operación elemental Unión con el conjunto que llama el método.
+     * @param c El Conjunto de tipo  génerico {@code T} con quien
+     * hacer la operación Unión.
+     * @return Un nuevo {@link ConjuntoArreglo} que contiene la unión
+     * de los dos conjuntos.
+     */
     public Conjunto<T> union(Conjunto<T> c) {
         T[] unionaux = UtilArreglos.crearArregloGenerico(elementos, c.obtenerCardinalidad() + elementos.length);
         int i = 0;
@@ -92,6 +143,17 @@ public class ConjuntoArreglo<T> extends Conjunto<T>{
         return new ConjuntoArreglo<>(union);
     }
 
+    /** 
+     * Método que devuelve un objeto génerico de tipo {@link ConjuntoArreglo}
+     * que contiene la intersección de dos conjuntos.
+     * El método recibe un {@link Conjunto} génerico con el
+     * cuál hacer la operación elemental intersección con el conjunto
+     * que manda a llamar el método.
+     * @param c El {@link Conjunto} génerico con el cúal hacer la operación
+     * intersección.
+     * @return Un objeto de tipo {@link ConjuntoArreglo} que contiene los elementos
+     * de la intersección de los dos conjuntos.
+    */
     public Conjunto<T> interseccion(Conjunto<T> c) {
         int n = Math.max(c.obtenerCardinalidad(), elementos.length);
         T[] interseccion = UtilArreglos.crearArregloGenerico(elementos, n);
@@ -106,9 +168,19 @@ public class ConjuntoArreglo<T> extends Conjunto<T>{
         for (int i = 0; i < m; i++) {
             inter[i] = interseccion[i];
         }
-        return new ConjuntoArreglo<>(inter);
+        return new ConjuntoArreglo<T>(inter);
     }
 
+    /** 
+     * Método que nos dice si dos conjuntos son iguales.
+     * Para que dos conjuntos sean iguales deben de tener el
+     * mismo tamaño de elementos y los mismos elementos en 
+     * cualquier orden. Si los dos conjuntos son iguales el 
+     * método regresa {@code true}, de otra forma regresa {@code false}.
+     * @param c El {@link Conjunto} a saber si son iguales.
+     * @return {@code true} Si los dos conjuntos son iguales.{@code false}
+     * de otra manera.
+    */
     public boolean iguales(Conjunto<T> c) {
         if (c.obtenerCardinalidad() != elementos.length) {
             return false;
@@ -122,6 +194,10 @@ public class ConjuntoArreglo<T> extends Conjunto<T>{
         return true;
     }
 
+    /**
+     * Método que devuelve la cardinalidad (tamaño) del {@link ConjuntoArreglo}.
+     * @return La cardinalidad del conjunto.
+     */
     public int obtenerCardinalidad(){
         return elementos.length;
     }
--- src/MainTextos.java
+++ src/MainTextos.java
@@ -8,14 +8,69 @@ public class MainTextos {
         String archivo2 = "elDicho.txt";
 
         try {
-            /*Aquí va tu código*/
+            ConjuntoArreglo<String> c1 = new ConjuntoArreglo<String>();
+            ConjuntoArreglo<String> c2 = new ConjuntoArreglo<String>();
+            BufferedReader br = new BufferedReader(new FileReader(archivo1));
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] partes = linea.trim().split("\\s+");
+                for (String palabra : partes) {
+                    palabra = palabra.replaceAll("[\\p{Punct}…]", "");
+                    if(!palabra.isEmpty()){
+                        c1.agregarElemento(palabra);
+                    }
+                }
+            }
+            
+            br.close();
+
+            BufferedReader br2 = new BufferedReader(new FileReader(archivo2));
+            String l;
+            while ((l = br2.readLine()) != null) {
+                String[] p = l.trim().split("\\s+");
+                for (String pal : p) {
+                    pal = pal.replaceAll("[\\p{Punct}…]", "");
+                    pal = pal.replace("¡", "");
+                    if(!pal.isEmpty()){
+                        c2.agregarElemento(pal);
+                    }
+                }
+            }
+
+            br2.close();
+
+            System.out.println("Las palabras sin repetir del archivo " + archivo1 + " son: ");
+
+            for (String string : c1) {
+                System.out.println(string);
+            }
+
+            System.out.println();
+
+            System.out.println("Las palabras sin repetir del archivo " + archivo2 + " son: ");
+
+            for (String s : c2) {
+                System.out.println(s);
+            }
+
+            System.out.println();
+
+            Conjunto<String> c3 = c1.interseccion(c2);
+
+            System.out.println("Las palabras que aparecen en ambos archivos son: ");
+
+            for (String string : c3) {
+                System.out.println(string);
+            }
+
 
         } catch (IOException e) {
             System.err.println("Error leyendo archivo: " + e.getMessage());
         } catch (IllegalArgumentException e) {
             System.err.println("Error creando conjuntos: " + e.getMessage());
         }
-    }
+            
+        }
 
     /*Aquí van tus métodos auxiliares en caso de necesitarlos*/
 }
